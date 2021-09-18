b7d94d74068598bd4ac567ec0017a73c2b0126ca
Stefan Hagen <stefan@hagen.link>
2021-09-18 11:52:57 +0200

    Update cli.py

--- koordinaati/cli.py
+++ koordinaati/cli.py
@@ -1,1 +1,15 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long
+"""Commandline API gateway for koordinaati."""
+import sys
+from typing import List, Union
 
+import koordinaati.koordinaati as coord
+
+
+# pylint: disable=expression-not-assigned
+def main(argv: Union[List[str], None] = None) -> int:
+    """Delegate processing to functional module."""
+    argv = sys.argv[1:] if argv is None else argv
+    return coord.main(argv)
