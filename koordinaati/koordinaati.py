121b2f5656d9d81cbd8128206fc9d00baac5cad4
Stefan Hagen <stefan@hagen.link>
2021-09-18 11:55:33 +0200

    Update koordinaati.py

--- koordinaati/koordinaati.py
+++ koordinaati/koordinaati.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""Coordinates API."""
+import os
+import sys
+from typing import List, Union
 
+
+DEBUG_VAR = 'KOORDINAATI_DEBUG'
+DEBUG = os.getenv(DEBUG_VAR)
+
+ENCODING = 'utf-8'
+ENCODING_ERRORS_POLICY = 'ignore'
+
+
+def main(argv: Union[List[str], None] = None) -> int:
+    """Drive the coordination."""
+    argv = argv if argv else sys.argv[1:]
+    if not argv:
+        print('ERROR arguments expected.', file=sys.stderr)
+        return 2
+    
+    return 0
