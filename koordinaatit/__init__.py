f065a129173b918863c13c2829ea58a0cc50920b
Stefan Hagen <stefan@hagen.link>
2023-10-22 12:19:45 +0200

    Bumped impl version for release 2023.10.22

--- koordinaatit/__init__.py
+++ koordinaatit/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.12.13+parent.ff7049ef'
-# [[[end]]] (checksum: 853a9f21e62834d23aa0601ad62a61ce)
+__version__ = '2023.10.22+parent.1ccad3f7'
+# [[[end]]] (checksum: 3c4b47bcb7a39a589a2d51e1a775f7f3)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
